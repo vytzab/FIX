0714984f3de84ef886b230ceb0d2af0689d3519a
vytzab <vytautas.zabielskas@gmail.com>
2023-12-18 14:04:03 +0200

    Tweaked GenerateID()

--- src/main/java/lt/vytzab/initiator/Order.java
+++ src/main/java/lt/vytzab/initiator/Order.java
@@ -42,7 +42,7 @@ public class Order implements Cloneable {
     }
 
     public String generateID() {
-        return Long.toString(System.currentTimeMillis()%10000 + (nextID++));
+        return Long.toString(100000 + (nextID++));
     }
 
     public SessionID getSessionID() {
